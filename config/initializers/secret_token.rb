df5e2cf2fda1d66e47320778ba8f94d21314770d
a5myth <zdelsh@mykolab.ch>
2013-10-29 16:02:40 +0000

    initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zworld::Application.config.secret_key_base = 'a46212c0ae893b1f97d89e95312d14589c54d60a09cef26c9b41f5f69e2ae4486f1a2ff8f7e81108dc1f3186c056656f99048c3df271d4d6e6814c3d6173e953'
